6abd3fdd3078739bda46d042e876b28adbc1efc8
Do Tien Dat <dotiendat711@gmail.com>
2025-04-07 23:13:47 +0700

    Tính năng đăng nhập, đăng ký của users

---
+++ server/routes/client/user.route.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+
+import * as controller from "../../controllers/client/user.controller";
+import { requireAuth } from "../../middlewares/client/user.middleware";
+
+router.post("/register", controller.register);
+
+router.post("/login", controller.login);
+
+router.get("/profile", requireAuth, controller.profile);
+
+export const userRoute = router;
